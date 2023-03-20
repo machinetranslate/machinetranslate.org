5945939ba235cb764e06576368ec66dcd78a491c
Cecilia Yalangozian <cyalangozian@gmail.com>
2023-03-20 19:09:44 -0300

    Include "self-serve" message in API template

--- generate.py
+++ generate.py
@@ -249,7 +249,7 @@ for api in APIS:
   urls = api['urls']
   privacy_url = api.get('privacy_url', None)
 
-  self_serve = api.get('self-serve', True)
+  self_serve = api.get('self-serve', "Unknown")
 
   customisation = []
   if api.get('adaptive', False):
